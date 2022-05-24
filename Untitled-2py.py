23f51bf99735ceea6d4408f6d186d43c4e160e13
kaya-mustf <106195274+kaya-mustf@users.noreply.github.com>
2022-05-25 00:09:42 +0300

    Add files via upload

---
+++ Untitled-2py.py
@@ -0,0 +1,27 @@
+
+sayı = int(input("bir sayı giriniz"))
+
+while sayı > 0:
+   print( sayı * sayı)
+   print(sayı)
+   if sayı == 0:
+       break
+  
+  
+       
+
+sayı = int()
+
+while(True):
+    sayı = int(input("bir sayı giriniz"))
+    print(f"sayı:",sayı, "karesi:",sayı*sayı)
+    if sayı == 0:
+        break
+
+  
+ 
+
+
+ 
+
+ 
